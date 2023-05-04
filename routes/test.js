b5aa175a9ef8c4174c5577c97984143f00be7864
zenith-noble01 <webdev686@gmail.com>
2023-05-04 17:52:06 +0100

    the register route was added without testing if it works or not

--- routes/test.js
+++ routes/test.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export const getUser = async (req, res) => {
   try {
@@ -13,5 +14,35 @@ export const getUser = async (req, res) => {
     return res.status(200).send(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      throw new Error("This Fields are required");
+    }
+
+    const userExists = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (userExists) {
+      throw new Error("User already exists with this credentials");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(200).send({ message: "User created succesfully." });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
   }
 };
