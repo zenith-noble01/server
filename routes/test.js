86583b3527d74524e2bdd03edcf15bf641742ca5
zenith-noble01 <webdev686@gmail.com>
2023-05-04 18:01:55 +0100

    the login route was added in the testRoute file but has not yet been added

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const getUser = async (req, res) => {
   try {
@@ -44,5 +45,41 @@ const createUser = async (req, res) => {
     res.status(200).send({ message: "User created succesfully." });
   } catch (error) {
     res.status(500).send({ error: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new Error("This Fields are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      throw new Error("Incorrect password");
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    res.status(200).send({ token: token, id: user._id });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
 };
